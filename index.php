736f9f0ebfa52b051ba29e4a539a2d660ae35340
codad5 <aniezeoformic@gmail.com>
2023-03-02 15:19:40 +0000

    testing

--- index.php
+++ index.php
@@ -5,10 +5,18 @@ $request_url = $_SERVER['REQUEST_URI'];
 $path = parse_url($request_url, PHP_URL_PATH); 
 $path = rtrim($path, '/');
 // var_dump($path);
-if (file_exists("download/".$path.".html") || file_exists("download/" . $path . ".css") || file_exists("download/" . $path . ".js") || $path == "") {
+if($path == "") $path = "index";
+if (file_exists("download/".$path.".html") || $path == "") {
     # code...
-    if($path == "") $path = "index";
     include_once "download/" . $path . ".html";
+}
+elseif (file_exists("download/" . $path . ".css")) {
+    # code...
+    include_once "download/" . $path . ".css";
+}
+elseif (file_exists("download/" . $path . ".js")) {
+    # code...
+    include_once "download/" . $path . ".js";
 }
 else{
     if (file_exists("download/404.html")) {
